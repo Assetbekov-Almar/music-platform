91e73415692566d2720cbffad4caa2b0c1c5320c
assetbekov-almar <almar.asetbekov@gmail.com>
2022-07-30 20:03:36 +0600

    Feature: Finish side bar ui

--- src/features/sidebar/Sidebar.tsx
+++ src/features/sidebar/Sidebar.tsx
@@ -5,6 +5,8 @@ import styles from '../../styles/Sidebar.module.scss'
 import logo from '../../assets/images/logo-with-text.svg'
 import Image from 'next/image'
 import Link from 'next/link'
+import { mainList } from './constants'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Sidebar = () => {
 	const dispatch = useAppDispatch()
@@ -19,14 +21,35 @@ const Sidebar = () => {
 	return (
 		<div className={styles.container}>
 			<Link href={'/'}>
-				<a>
+				<a className={styles.logo__link}>
 					<Image src={logo} alt={'logo'} className={styles.logo} />
 				</a>
 			</Link>
 
-			{playlists.map((item) => (
-				<div key={item.id}>{item.name}</div>
-			))}
+			<ul className={styles.main_links}>
+				{mainList.map((item) => (
+					<li key={item.title}>
+						<Link href={item.route}>
+							<a>
+								<FontAwesomeIcon icon={item.icon} className={styles.link__icon} />
+								<span className={styles.link__title}>{item.title}</span>
+							</a>
+						</Link>
+					</li>
+				))}
+			</ul>
+
+			<hr />
+
+			<ul className={styles.playlist__links}>
+				{playlists.map((item) => (
+					<li key={item.id}>
+						<Link href={''}>
+							<a>{item.name}</a>
+						</Link>
+					</li>
+				))}
+			</ul>
 		</div>
 	)
 }
---
+++ src/features/sidebar/constants.ts
@@ -0,0 +1,9 @@
+import { faHouse } from '@fortawesome/free-solid-svg-icons/faHouse'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass'
+import { faBook } from '@fortawesome/free-solid-svg-icons/faBook'
+
+export const mainList = [
+	{ title: 'Home', icon: faHouse, route: '' },
+	{ title: 'Search', icon: faMagnifyingGlass, route: '' },
+	{ title: 'Your Library', icon: faBook, route: '' },
+]
